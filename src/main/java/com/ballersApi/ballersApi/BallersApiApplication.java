70222cafb79a9ec4b1e8083fb0a35afcc21c476a
Mohammed <202110238@students.asu.edu.jo>
2025-02-12 21:36:28 +0300

    first Commit

---
+++ src/main/java/com/ballersApi/ballersApi/BallersApiApplication.java
@@ -0,0 +1,13 @@
+package com.ballersApi.ballersApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BallersApiApplication.class, args);
+	}
+
+}
