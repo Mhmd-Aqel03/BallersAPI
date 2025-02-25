345d137afb848014c8e0a1d10aa64c137d398fdd
Abdelrhman <145473949+Abd-AbuSabra@users.noreply.github.com>
2025-02-25 19:43:03 +0300

    Update UserController.java
    added the validation annotations

--- src/main/java/com/ballersApi/ballersApi/controllers/UserController.java
+++ src/main/java/com/ballersApi/ballersApi/controllers/UserController.java
@@ -1,4 +1,10 @@
 package com.ballersApi.ballersApi.controllers;
 
+import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+
+@Valid
+@Validated
 public class UserController {
 }
