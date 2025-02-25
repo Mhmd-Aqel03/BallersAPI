9821bc03080a2d744241dddbd88f52622049f73c
Abdelrhman <145473949+Abd-AbuSabra@users.noreply.github.com>
2025-02-25 19:32:59 +0300

    Update Session.java
    added validation

--- src/main/java/com/ballersApi/ballersApi/models/Session.java
+++ src/main/java/com/ballersApi/ballersApi/models/Session.java
@@ -1,8 +1,12 @@
 package com.ballersApi.ballersApi.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
 import java.time.LocalDateTime;
 
@@ -14,11 +18,22 @@ public class Session {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+
+  @NotBlank(message = "Session type can't be empty")
+    @Column(nullable = false)
     private String type;
 
+
+    @NotNull
+    @Column(nullable = false)
     // Time stamp for the date and time
     private LocalDateTime matchDateTime;
 
+
+   @NotNull
+    @Column(nullable = false)
+   @Min(value = 6, message = "Max players must be at least 2")
+   @Max(value = 12, message = "Max players cannot exceed 12")
     private int maxPlayers;
 
     private int playerCount;
