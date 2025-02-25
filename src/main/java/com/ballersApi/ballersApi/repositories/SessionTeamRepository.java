0a7a96f4420ba67e72c1dba1480e4db283d332ad
Abdelrhman <145473949+Abd-AbuSabra@users.noreply.github.com>
2025-02-25 19:35:29 +0300

    Add files via upload

---
+++ src/main/java/com/ballersApi/ballersApi/repositories/SessionTeamRepository.java
@@ -0,0 +1,12 @@
+package com.ballersApi.ballersApi.repositories;
+
+import com.ballersApi.ballersApi.models.Session;
+import com.ballersApi.ballersApi.models.SessionTeam;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface SessionTeamRepository extends JpaRepository<SessionTeam, Long> {
+    List<SessionTeam> findBySession(Session session);
+}
