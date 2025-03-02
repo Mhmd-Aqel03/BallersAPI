5e54a5239df8a0bf120d6265b321530cc52e0aae
qqudah <mohannadqqudah@gmail.com>
2025-03-02 14:12:46 +0300

    fixed the merge conflict

--- src/main/java/com/ballersApi/ballersApi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ballersApi/ballersApi/exceptions/GlobalExceptionHandler.java
@@ -1,14 +1,47 @@
 package com.ballersApi.ballersApi.exceptions;
 
 
-import com.ballersApi.ballersApi.models.Court;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    //  This Handles Custom Exception, User not found in this case.
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleUserNotFoundException(Exception ex) {
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    //  User creation Error, wasn't sure what to call it.
+    @ExceptionHandler(UserCreationErrorException.class)
+    public ResponseEntity<Map<String, Object>> handleUserCreationErrorException(Exception ex) {
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    //  This Handles all "Non Custom" error, or internal server errors.
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleGeneralException(Exception ex) {
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     @ExceptionHandler(CourtIdNotFoundException.class)
     public ResponseEntity<String> handleCourtIdNotFoundException(CourtIdNotFoundException e) {
