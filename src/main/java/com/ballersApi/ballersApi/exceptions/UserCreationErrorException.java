0897f2c64de88656b752825163ed2605be44d441
Abdelrhman <145473949+Abd-AbuSabra@users.noreply.github.com>
2025-03-02 14:15:55 +0300

    Update UserCreationErrorException.java

--- src/main/java/com/ballersApi/ballersApi/exceptions/UserCreationErrorException.java
+++ src/main/java/com/ballersApi/ballersApi/exceptions/UserCreationErrorException.java
@@ -1,5 +1,4 @@
 package com.ballersApi.ballersApi.exceptions;
-
 public class UserCreationErrorException extends RuntimeException {
     public UserCreationErrorException(String message) {
         super(message);
