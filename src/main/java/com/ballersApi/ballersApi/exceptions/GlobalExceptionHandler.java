c05b6d14d45c3726321ad7f83c4bf908395980a4
Abdelrhman <145473949+Abd-AbuSabra@users.noreply.github.com>
2025-03-02 14:16:18 +0300

    Update GlobalExceptionHandler.java

--- src/main/java/com/ballersApi/ballersApi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/ballersApi/ballersApi/exceptions/GlobalExceptionHandler.java
@@ -1,13 +1,10 @@
-
 package com.ballersApi.ballersApi.exceptions;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.util.HashMap;
 import java.util.Map;
 
