eadfd18849bed5204fb0d45f4fc67e763edbc44a
Abdelrhman <145473949+Abd-AbuSabra@users.noreply.github.com>
2025-03-03 02:41:12 +0300

    Update SessionController.java
    updated delete session

--- src/main/java/com/ballersApi/ballersApi/controllers/SessionController.java
+++ src/main/java/com/ballersApi/ballersApi/controllers/SessionController.java
@@ -1,22 +1,15 @@
 package com.ballersApi.ballersApi.controllers;
 
-import com.ballersApi.ballersApi.models.Player;
 import com.ballersApi.ballersApi.models.Session;
 import com.ballersApi.ballersApi.models.SessionTeam;
-import com.ballersApi.ballersApi.repositories.SessionRepository;
 import com.ballersApi.ballersApi.services.SessionService;
 import com.ballersApi.ballersApi.services.SessionTeamService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 
@@ -36,14 +29,18 @@ public class SessionController {
     public SessionTeam createSession(@Valid @RequestBody Session session){
 
         sessionService.createSession(session);
+        sessionTeamService.createTeamSession(session.getId());
         return sessionTeamService.createTeamSession(session.getId());
 
 
 
     }
-    @DeleteMapping("deleteSession")
-    public void deleteSession(@Valid @RequestBody Session session){
-        sessionService.deleteSession(session);
+    @DeleteMapping("deleteSession/{id}")
+    public ResponseEntity<Void> deleteSession(@PathVariable  Long id){
+
+        sessionTeamService.deleteAllTeamSessions();
+        sessionService.deleteSession(id);
+        return ResponseEntity.ok().build();
     }
 
     @PostMapping("createSessionTeam")
@@ -63,8 +60,8 @@ public class SessionController {
         return ResponseEntity.badRequest().build();
     }
     @DeleteMapping("/deleteSessionTeam/{id}")
-    public ResponseEntity<Void> deleteTeamSession(@PathVariable Long id) {
-        sessionTeamService.deleteTeamSession(id);
+    public ResponseEntity<Void> deleteTeamSession() {
+        sessionTeamService.deleteAllTeamSessions();
         return ResponseEntity.ok().build();
     }
     @GetMapping("/getTeam/{sessionId}")
