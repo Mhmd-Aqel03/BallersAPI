235b86af4217d69b0b171007263dc947a799ced1
Mohammed <mhmdaqelbs@gmail.com>
2025-03-03 23:19:31 +0300

    edit: Apllication prints port now

--- src/main/java/com/ballersApi/ballersApi/BallersApiApplication.java
+++ src/main/java/com/ballersApi/ballersApi/BallersApiApplication.java
@@ -4,6 +4,7 @@ import com.ballersApi.ballersApi.models.Player;
 import com.ballersApi.ballersApi.models.User;
 import com.ballersApi.ballersApi.repositories.PlayerRepository;
 import com.ballersApi.ballersApi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,18 +13,21 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class BallersApiApplication {
 
+	@Value("${server.port}")
+	private String serverPort;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BallersApiApplication.class, args);
 	}
 
 	@Bean
-	public static CommandLineRunner commandLineRunner(
+	public CommandLineRunner commandLineRunner(
 			UserRepository userRepository
 	) {
 		return args -> {
 
 
-			System.out.println("Server running on port 5000");
+			System.out.println("Server running on port " + serverPort);
 		};
 	}
 
