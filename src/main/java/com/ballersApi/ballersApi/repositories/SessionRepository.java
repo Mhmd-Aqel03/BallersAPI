49ef91ea3b35c96292b27feb63bf78fcbd93c216
Abdelrhman <145473949+Abd-AbuSabra@users.noreply.github.com>
2025-04-06 18:57:00 +0300

    Update SessionRepository.java

--- src/main/java/com/ballersApi/ballersApi/repositories/SessionRepository.java
+++ src/main/java/com/ballersApi/ballersApi/repositories/SessionRepository.java
@@ -4,9 +4,12 @@ import com.ballersApi.ballersApi.models.Session;
 import com.ballersApi.ballersApi.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface SessionRepository extends JpaRepository<Session, Long> {
-    List<Session> findBymatchDateTimeAfter(LocalDateTime now);
+    List<Session> findByMatchDateAfter(LocalDate now);
+    List<Session> findByMatchDateBetween(LocalDate startDate, LocalDate endDate);
+
 }
