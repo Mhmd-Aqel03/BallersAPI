227fa6c4591c5cfac5170e2f6585370e6807fd1d
Mohammed <mhmdaqelbs@gmail.com>
2025-04-07 18:16:02 +0300

    removed referee

--- src/main/java/com/ballersApi/ballersApi/models/Session.java
+++ src/main/java/com/ballersApi/ballersApi/models/Session.java
@@ -6,13 +6,11 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Entity
@@ -24,14 +22,11 @@ public class Session {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-
-
-  @NotNull(message = "Session type can't be empty")
+    @NotNull(message = "Session type can't be empty")
     @Column(nullable = false)
     private SessionType type;
 
     @NotNull(message = "matchDate can't be null")
-
     @Column(nullable = false)
     // Time stamp for the date
     private LocalDate matchDate;
@@ -40,7 +35,6 @@ public class Session {
     @Column(nullable = false)
     // Time stamp for the start of the session
     private LocalTime matchStartTime;
-
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "hh:mm a")
     @NotNull(message = "matchEndTime can't be null")
@@ -51,8 +45,8 @@ public class Session {
     @NotNull(message = "maxPlayers can't be null")
     @Column(nullable = false)
 
-   @Min(value = 2, message = "Max players must be at least 2")
-   @Max(value = 10, message = "Max players cannot exceed 10")
+    @Min(value = 2, message = "Max players must be at least 2")
+    @Max(value = 10, message = "Max players cannot exceed 10")
 
     private int maxPlayers;
 
@@ -62,15 +56,12 @@ public class Session {
     @Max(value = 10, message = "Max players cannot exceed 12")
     private int playerCount = 0;
 
-
     @ManyToOne()
     @JoinColumn(name = "court_id")
     private Court court;
 
     @ManyToOne()
     @JoinColumn(name = "referee_id")
-    private Referee referee;
-
-
+    private User referee;
 
 }
