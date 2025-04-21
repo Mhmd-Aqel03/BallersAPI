e7fa9ea61ba0dccc2bbfacb1121982faf37fea42
Abdelrhman <145473949+Abd-AbuSabra@users.noreply.github.com>
2025-04-22 00:56:22 +0300

    Update SessionController.java

--- src/main/java/com/ballersApi/ballersApi/controllers/SessionController.java
+++ src/main/java/com/ballersApi/ballersApi/controllers/SessionController.java
@@ -1,25 +1,28 @@
 package com.ballersApi.ballersApi.controllers;
+
+import com.ballersApi.ballersApi.dataTransferObjects.SessionDTO;
 import com.ballersApi.ballersApi.dataTransferObjects.SessionTeamDTO;
 import com.ballersApi.ballersApi.models.Session;
 import com.ballersApi.ballersApi.models.SessionTeam;
+import com.ballersApi.ballersApi.models.Team;
 import com.ballersApi.ballersApi.services.SessionService;
 import com.ballersApi.ballersApi.services.SessionTeamService;
 import com.ballersApi.ballersApi.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestController
 
 
-@RequestMapping("/session")
+@RequestMapping("/Session")
 public class SessionController {
     @Autowired
     private SessionService sessionService;
@@ -27,14 +30,9 @@ public class SessionController {
     private SessionTeamService sessionTeamService;
     @Autowired
     private UserService userService;
-
-    @GetMapping("/getSessions")
-    public ResponseEntity<Map<String,Object>> getAllUpcomingSessions(){
-        Map<String, Object> response = new HashMap<>();
-
-        response.put("sessions", sessionService.getAllUpcomingSessions());
-
-        return ResponseEntity.ok(response);
+    @GetMapping("getAllSessions")
+    public List<Session> getAllUpcomingSessions(){
+        return sessionService.getAllUpcomingSessions();
     }
 
     @GetMapping("getSession/{id}")
@@ -49,27 +47,57 @@ public class SessionController {
         return sessionService.getSessionsForWeek(Date);
     }
 
-    @PostMapping("joinSessionTeam/{teamSessionId}")
-    public ResponseEntity<SessionTeam> joinTeamSession(@PathVariable Long teamSessionId) {
+    @PostMapping("createSession")
+    public Session createSession(@Valid @RequestBody SessionDTO request) {
+        Session session = new Session();
+        session.setType(request.getType());
+        session.setMatchDate(request.getMatchDate());
+        session.setMatchStartTime(request.getMatchStartTime());
+        session.setMatchEndTime(request.getMatchEndTime());
+        session.setMaxPlayers(request.getMaxPlayers());
+        session.setPrice(request.getPrice());
+        session.setPlayerCount(0);;
+
+
+        sessionService.createSession(session);
+        sessionTeamService.createTeamSession(session.getId());
+        sessionTeamService.createTeamSession(session.getId());
+        return session;
+
+    }
+    @DeleteMapping("deleteSession/{id}")
+
+    public ResponseEntity<Void> deleteSession(@PathVariable  Long id){
+
+        sessionTeamService.deleteAllTeamSessions(id);
+        sessionService.deleteSession(id);
+        return ResponseEntity.ok().build();
+    }
+    @PostMapping("joinSessionTeam/{sessionId}/{team}")
+    public ResponseEntity<SessionTeam> joinTeamSession(@PathVariable Long sessionId, @PathVariable Team team) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         Long playerId = userService.getUserByUsername(username).getId();
-        SessionTeam teamSession = sessionTeamService.joinTeamSession(teamSessionId, playerId);
+        SessionTeam teamSession = sessionTeamService.joinTeamSession(sessionId, playerId, team);
+
         if (teamSession != null) {
             return ResponseEntity.ok(teamSession);
         }
         return ResponseEntity.badRequest().build();
     }
-
-    @PostMapping("leaveSessionTeam/{teamId}")
-    public ResponseEntity<String> leaveTeam(@PathVariable Long teamId) {
+    @PostMapping("leaveSessionTeam/{sessionId}/{team}")
+    public ResponseEntity<String> leaveTeam(@PathVariable Long sessionId,@PathVariable Team team) {
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
         Long playerId = userService.getUserByUsername(username).getId();
-        sessionTeamService.leaveTeam(playerId, teamId);
+        sessionTeamService.leaveTeam(playerId, sessionId, team);
         return ResponseEntity.ok("Player removed from the team successfully");
     }
-
+    
     @GetMapping("/getTeam/{sessionId}")
     public ResponseEntity<List<SessionTeamDTO>> getTeamsBySession(@PathVariable Long sessionId) {
         return ResponseEntity.ok(sessionTeamService.getTeamsBySession(sessionId));
     }
+
+
+
+
 }
