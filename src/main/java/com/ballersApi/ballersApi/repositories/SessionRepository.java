57f1c6b0ddcd6a7a59c71476f00a1f1db68c3fe3
Mohammed <mhmdaqelbs@gmail.com>
2025-04-30 09:39:52 +0300

    made delte courts delete all sessions

--- src/main/java/com/ballersApi/ballersApi/repositories/SessionRepository.java
+++ src/main/java/com/ballersApi/ballersApi/repositories/SessionRepository.java
@@ -1,5 +1,6 @@
 package com.ballersApi.ballersApi.repositories;
 
+import com.ballersApi.ballersApi.models.Court;
 import com.ballersApi.ballersApi.models.Session;
 import com.ballersApi.ballersApi.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,5 @@ public interface SessionRepository extends JpaRepository<Session, Long> {
     Optional<Session> findById(Long id);
     List<Session> findByMatchDateAfter(LocalDate now);
     List<Session> findByMatchDateBetween(LocalDate startDate, LocalDate endDate);
+    void deleteAllByCourt(Court court);
 }
