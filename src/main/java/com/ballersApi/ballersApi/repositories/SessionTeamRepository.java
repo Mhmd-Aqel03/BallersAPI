74087250bf7e57bdc840f3c4a011c806607b783f
Abdelrhman <145473949+Abd-AbuSabra@users.noreply.github.com>
2025-05-02 00:14:26 +0300

    Update SessionTeamRepository.java

--- src/main/java/com/ballersApi/ballersApi/repositories/SessionTeamRepository.java
+++ src/main/java/com/ballersApi/ballersApi/repositories/SessionTeamRepository.java
@@ -2,14 +2,14 @@ package com.ballersApi.ballersApi.repositories;
 
 import com.ballersApi.ballersApi.models.Session;
 import com.ballersApi.ballersApi.models.SessionTeam;
+import com.ballersApi.ballersApi.models.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
 public interface SessionTeamRepository extends JpaRepository<SessionTeam, Long> {
-    List<SessionTeam> findBySession(Session session);
-    List<SessionTeam> findBySessionId(Long sessionId);
-    List<SessionTeam> id(Long id);
-    void deleteSessionTeamBySessionId(Long sessionId);
+
+
+
 }
