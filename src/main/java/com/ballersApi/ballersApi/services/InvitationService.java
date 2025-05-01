33411de13405846de481585511b5a399ca10204c
Abdelrhman <145473949+Abd-AbuSabra@users.noreply.github.com>
2025-05-02 00:15:37 +0300

    Update InvitationService.java

--- src/main/java/com/ballersApi/ballersApi/services/InvitationService.java
+++ src/main/java/com/ballersApi/ballersApi/services/InvitationService.java
@@ -34,9 +34,17 @@ public class InvitationService {
                 .orElseThrow(() -> new PlayerNotFoundException("Player with id " + receiverId + " not found"));
         Session session = sessionRepository.findById(sessionId)
                 .orElseThrow(() -> new TeamSessionNotFoundException("Team session with id " + sessionId + " not found"));
+
+        boolean isInSession = player.getSessionTeams().stream()
+                .anyMatch(team1 -> session.getId().equals(sessionId));
+
+        if (!isInSession) {
+            throw new PlayerNotFoundException("Player "+ playerId + " is not in session");
+        }
         if(!session.getType().equals(SessionType.Random)){
             throw new WrongInvitationTypeException("Session type doesn't match Invitation type");
         }
+
 
 
         Invitation invite = new Invitation();
