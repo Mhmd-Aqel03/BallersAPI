9813537e1d680f2554784de6284b5ba674f7d065
Abdelrhman <145473949+Abd-AbuSabra@users.noreply.github.com>
2025-05-05 00:05:55 +0300

    Update PlayerHistoryDTO.java

--- src/main/java/com/ballersApi/ballersApi/dataTransferObjects/PlayerHistoryDTO.java
+++ src/main/java/com/ballersApi/ballersApi/dataTransferObjects/PlayerHistoryDTO.java
@@ -1,16 +1,15 @@
 package com.ballersApi.ballersApi.dataTransferObjects;
 
-import com.ballersApi.ballersApi.models.Session;
 import lombok.Data;
-
 
 @Data
 public class PlayerHistoryDTO {
     private String result;
     private SessionDTO sessionDetails;
 
-    public PlayerHistoryDTO(Session session, boolean won) {
+    // Constructor to accept the sessionDTO directly
+    public PlayerHistoryDTO(SessionDTO sessionDetails, boolean won) {
         this.result = won ? "won" : "lost";
-        this.sessionDetails = new SessionDTO(session);
+        this.sessionDetails = sessionDetails;
     }
 }
