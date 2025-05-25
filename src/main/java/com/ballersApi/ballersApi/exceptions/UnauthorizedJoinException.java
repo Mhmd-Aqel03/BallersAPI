b728deff71064901714e63f2ec37ab5f7d9e574b
Abdelrhman <145473949+Abd-AbuSabra@users.noreply.github.com>
2025-05-25 17:07:47 +0300

    Add files via upload

---
+++ src/main/java/com/ballersApi/ballersApi/exceptions/UnauthorizedJoinException.java
@@ -0,0 +1,7 @@
+package com.ballersApi.ballersApi.exceptions;
+
+public class UnauthorizedJoinException extends RuntimeException {
+    public UnauthorizedJoinException(String message) {
+        super(message);
+    }
+}
