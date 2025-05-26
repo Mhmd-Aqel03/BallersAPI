2850571757a8d01cf90cf2465757271a8f6657fc
Abdelrhman <145473949+Abd-AbuSabra@users.noreply.github.com>
2025-05-26 16:25:04 +0300

    Update InvitationRepository.java

--- src/main/java/com/ballersApi/ballersApi/repositories/InvitationRepository.java
+++ src/main/java/com/ballersApi/ballersApi/repositories/InvitationRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface InvitationRepository extends JpaRepository<Invitation, Long> {
-    List<Invitation> findByReceiverAndStatus(Player receiver, boolean status);
+    List<Invitation> findByReceiverId(Long receiverId);
 }
