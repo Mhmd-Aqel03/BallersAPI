ff56138276fa613c7423c1ede3dd8d4ec11e81ec
Abdelrhman <145473949+Abd-AbuSabra@users.noreply.github.com>
2025-06-13 19:17:36 +0300

    Add files via upload

---
+++ src/main/java/com/ballersApi/ballersApi/exceptions/InvitationSentException.java
@@ -0,0 +1,7 @@
+package com.ballersApi.ballersApi.exceptions;
+
+public class InvitationSentException extends RuntimeException {
+    public InvitationSentException(String message) {
+        super(message);
+    }
+}
